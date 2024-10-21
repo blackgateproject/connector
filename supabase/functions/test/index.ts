4668db850519d6fc647c97200ca3bc3f96598341
Awais Shahid <i212764@nu.edu.pk>
2024-10-22 01:44:43 +0500

    WIP: Supabase migration in full swing

---
+++ supabase/functions/test/index.ts
@@ -0,0 +1,59 @@
+import { createClient } from 'jsr:@supabase/supabase-js@2'
+
+console.log(`[EDGE]: Function up and running!`)
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+        "authorization, x-client-info, apikey, content-type",
+};
+Deno.serve(async (req: Request) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  try {
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get('SUPABASE_URL') ?? '',
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      {
+        global: {
+          headers: { Authorization: req.headers.get('Authorization')! },
+        },
+      }
+    )
+
+    console.log(`[EDGE]: Supabase URL: ${Deno.env.get('SUPABASE_URL')}`)
+    console.log(`[EDGE]: Supabase ANON KEY: ${Deno.env.get('SUPABASE_ANON_KEY')}`)
+
+    // First get the token from the Authorization header
+    const token = req.headers.get('Authorization').replace('Bearer ', '')
+
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser(token)
+
+    // And we can run queries in the context of our authenticated user
+    const { data, error } = await supabaseClient.from('supaChain').select('*')
+    if (error) throw error
+
+    return new Response(JSON.stringify({ data }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
+
+// To invoke:
+//curl -i --location --request POST 'http://localhost:54321/functions/v1/test' --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU' --header 'Content-Type: application/json' --data '{"name":"Functions"}'
