cb6bffdf9be399ad65c669c4365ef53ddd3452b1
Awais Shahid <i212764@nu.edu.pk>
2025-01-25 21:04:04 +0500

    ADDED:: Wallet address + private key env vars

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     HARDHAT_URL: str
+    BLOCKCHAIN_WALLET_PRVT_KEY: str
+    BLOCKCHAIN_WALLET_ADDR: str
     SUPABASE_URL: str
     SUPABASE_ANON_KEY: str
     SUPABASE_SERV_KEY: str
