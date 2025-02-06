b7f107b52d6e0fe29ab906ead55cc6fbd4d9fa80
Awais Shahid <i212764@nu.edu.pk>
2025-02-06 14:05:46 +0500

    added merkle utils

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,8 @@
 from .accumulator_utils import *
 from .core_utils import *
 from .ipfs_utils import *
+from .merkle_utils import *
 from .pki_utils import *
 from .web3_utils import *
+
 # from .nonIPFS_web3_utils import *
