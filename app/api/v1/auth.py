de0d856fcd5fdaf2c9d2a543d3ef649b12c3518a
Awais Shahid <i212764@nu.edu.pk>
2025-03-06 20:47:46 +0500

    added register endpoint

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -15,11 +15,6 @@ from ...utils.core_utils import (
     settings_dependency,
     verify_jwt,
 )
-# from ...utils.pki_utils import (
-#     create_signing_challenge,
-#     sign_challenge,
-#     verify_signing_challenge,
-# )
 from ...utils.web3_utils import verifyUserOnMerkle
 
 router = APIRouter()
@@ -34,6 +29,59 @@ challenges = {}
 @router.get("/")
 async def health_check():
     return "Reached Auth Endpoint, Router Auth is Active"
+
+
+@router.post("/register")
+async def register(request: Request, settings: settings_dependency):
+    """
+    User self-generates a DID, signs a VC with it and sends it here to be verified
+    Takes wallet_address, didString, verifiableCredential
+    :param request:
+    :return:
+    """
+    body = await request.json()
+    wallet_address = body.get("wallet_address")
+    didString = body.get("didStr")
+    verifiableCredential = body.get("verifiableCredential")
+
+    # Print the request body
+    if debug:
+        print(f"Wallet Address: {wallet_address}")
+        print(f"DID String: {didString}")
+        print(f"Verifiable Credential: {verifiableCredential}")
+
+    # Add details to supabase table "requests"
+    supabase: Client = create_client(
+        supabase_url=settings.SUPABASE_URL, supabase_key=settings.SUPABASE_ANON_KEY
+    )
+    if supabase:
+        try:
+            # Add the request to the supabase table
+            request = supabase.table("requests").insert(
+                [
+                    {
+                        "wallet_addr": wallet_address,
+                        "did_str": didString,
+                        "verifiable_cred": verifiableCredential,
+                    }
+                ]
+            ).execute()
+            # Print the request data
+            if debug:
+                print(f"Request Data: {request.data}")
+
+            # Return authenticated response
+            return JSONResponse(
+                content={"authenticated": True, "message": "Request added to DB"},
+                status_code=200,
+            )
+        except Exception as e:
+            print(f"Error: {e}")
+            return JSONResponse(
+                content={"authenticated": False, "error": str(e)}, status_code=500
+            )
+    else:
+        raise Exception("[ERROR]: Supabase client not created")
 
 
 # Using request.json() to get the request body, forces async to be used. Need to
