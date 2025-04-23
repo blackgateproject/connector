031b667f6be7305cbee16d84788f51bd90e9af1a
Awais Shahid <i212764@nu.edu.pk>
2025-04-23 11:04:39 +0500

    trying to gett deployment script docker to run

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     BLOCKCHAIN_RPC_URL: str
-    # ZK_HARDHAT_URL : str
     BLOCKCHAIN_WALLET_PRVT_KEY: str
     BLOCKCHAIN_WALLET_ADDR: str
     SUPABASE_URL: str
@@ -17,10 +16,3 @@ class Settings(BaseSettings):
     ZKSYNC_NODE_TYPE: str
     BLOCKCHAIN_CHAIN_ID: int
     CRED_SERVER_URL: str
-    # BACKEND_DID: str
-    # BACKEND_JWK: str
-    # BACKEND_ACC: int
-    # BACKEND_MODULUS: int
-    # IPFS_SWARM_KEY: str
-    # IPFS_API_URL: str
-    # ENABLE_IPFS: bool
